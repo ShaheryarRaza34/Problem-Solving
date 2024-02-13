3e515e32a09cd59490ca0f95c9b95d420190a2c9
SharyR <shaheryarraza34@gmail.com>
2024-02-13 17:06:50 +0500

    feat: added first_ missing_positive_hard

--- Problem Solving Practice Python/main.py
+++ Problem Solving Practice Python/main.py
@@ -2,9 +2,11 @@ from problems.CoinChange import coinChange
 from problems.LIS import longestIncreasingSubsequence
 from problems.LongestPalindromicSubsequence import longestPalindromeSubseq
 from problems.LongestCommonSubsequence import longestCommonSubseq
+from problems.FirstMissingPositive import firstMissingPositive
 
 
 print(coinChange([1,5,7,9,11], 16))
 print(longestIncreasingSubsequence([3,2,6,4,5,1]))
 print(longestPalindromeSubseq('cbbd'))
 print(longestCommonSubseq('abcde', 'ace' ))
+print(firstMissingPositive([1,2,0]))
