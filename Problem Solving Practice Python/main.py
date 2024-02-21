3475982f244d548b7898f714081dcd86813ddaea
SharyR <shaheryarraza34@gmail.com>
2024-02-21 13:45:00 +0500

    feat: added best_time_to_sell_stock_1

--- Problem Solving Practice Python/main.py
+++ Problem Solving Practice Python/main.py
@@ -6,6 +6,7 @@ from problems.FirstMissingPositive import firstMissingPositive
 from problems.InsertIntoBST import insertIntoBST
 from problems.MinimumTimeToCollectApples import minTime
 from problems.BoatsToSavePeople import numRescueBoats
+from problems.BestTimeToSellStockI import maxProfit
 
 print(coinChange([1,5,7,9,11], 16))
 print(longestIncreasingSubsequence([3,2,6,4,5,1]))
@@ -15,3 +16,4 @@ print(firstMissingPositive([1,2,0]))
 print(insertIntoBST(None,2 ))
 print(minTime(7, [[0,1],[0,2],[1,4],[1,5],[2,3],[2,6]],[False,True,True,False,True,True,False]))
 print(numRescueBoats([3,2,2,1], 3))
+print(maxProfit([7,1,5,3,6,4]))
